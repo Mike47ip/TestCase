0a0ca014d972a9a6578424ca2f436e520447bc16
MIKE <agyemankojo47@gmail.com>
2023-07-27 19:08:18 +0000

    Update stringLength.test.js

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,4 +1,4 @@
-const stringLength = require('./stringLength');
+const stringLength = require('./stringLength.js');
 
 test('returns character count of the string', () => {
   const inputString = 'Hello, world!';
