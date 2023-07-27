c7e0b4820137cd0585070ee42a34e4c2b36f4dd2
MIKE <agyemankojo47@gmail.com>
2023-07-27 19:17:10 +0000

    Update stringLength.test.js

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,7 +1,20 @@
-const stringLength = require('./stringLength.js');
+// Updated stringLength.test.js
+const stringLength = require('./stringLength');
 
 test('returns character count of the string', () => {
   const inputString = 'Hello, world!';
   const result = stringLength(inputString);
   expect(result).toBe(inputString.length);
+});
+
+test('throws an error for an empty string', () => {
+  expect(() => {
+    stringLength('');
+  }).toThrow('String length must be between 1 and 10 characters.');
+});
+
+test('throws an error for a string longer than 10 characters', () => {
+  expect(() => {
+    stringLength('This string is longer than 10 characters.');
+  }).toThrow('String length must be between 1 and 10 characters.');
 });
